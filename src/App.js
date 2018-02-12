b188f9eb0f5961f3d7587039a7dc24dcd6f08c82
unknown <jordanmbush@gmail.com>
2018-02-12 14:49:38 -0700

    completed Step 4 - connected App to store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import router from './router'
+import { connect } from 'react-redux';
 
 
 class App extends Component {
@@ -143,14 +144,12 @@ class App extends Component {
   render() {
     return (
       <div>
-    
-        { 
-          router 
-        }
-
+        { router }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProp = state => state;
+
+export default connect(mapStateToProp)(App);
